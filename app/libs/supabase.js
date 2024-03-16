c0f6014ac01a8675b3cb0830a4426094e3b9708e
Julian <julialva2008@gmail.com>
2024-03-16 20:08:40 -0300

    env

--- app/libs/supabase.js
+++ app/libs/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 
 const supabaseUrl = 'https://snpzufisarbliaevsggm.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY ?? '1';
 export const supabase = createClient(supabaseUrl, supabaseKey);
